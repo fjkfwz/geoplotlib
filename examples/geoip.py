700daba8b5b25ccb968164230e686900452ddcb6
fjkfwz <fjkfwz@gmail.com>
2016-05-01 16:34:26 +0800

    添加毕业论文代码

--- examples/geoip.py
+++ examples/geoip.py
@@ -3,17 +3,22 @@
 import csv
 import random
 import numpy as np
+import matplotlib as mpl
 import geoplotlib
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+
 from matplotlib.ticker import NullFormatter
 from matplotlib.mlab import csv2rec
 from matplotlib.cbook import get_sample_data
 from geoplotlib.utils import BoundingBox
 from mapapi import baidu
-from collections import defaultdict
+from collections import defaultdict, Counter
 from geoplotlib.utils import DataAccessObject
+
+mpl.rcParams['font.sans-serif'] = ['Inziu Iosevka SC']  # 指定默认字体
+mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
 
 
 class GetGip(object):
@@ -46,13 +51,19 @@ class GetGip(object):
 
 
 class CaculateDateAndSave(object):
+    SOUTHERN_AREA =  ['江苏','安徽','江西','湖北','湖南','云南','贵州','四川','重庆','广西','广东','福建','浙江','上海','台湾','海南','香港','澳门']
+    NOTHERN_AREA = ['山东','山西','陕西','河北','河南','甘肃','宁夏','青海','西藏','新疆','内蒙古','辽宁','吉林','黑龙江','北京','天津']
     def __init__(self, source_file, output_filename):
         self.source_file = source_file
         self.normalization_lon_haplogroup_dict = defaultdict(lambda: defaultdict(int))
         self.normalization_lat_haplogroup_dict = defaultdict(lambda: defaultdict(int))
         self.lat_haplogroup_dict = defaultdict(lambda: defaultdict(int))
         self.lon_haplogroup_dict = defaultdict(lambda: defaultdict(int))
+        self.province_dict = defaultdict(lambda: defaultdict(int))
         self.ouput_filename = output_filename
+        self.ethnic_dict = defaultdict(lambda: defaultdict(int))
+        self.normalization_province_dict = defaultdict(lambda: defaultdict(int))
+        self.area_dict= defaultdict(lambda :defaultdict(int))
 
     def load_data(self):
         with open(self.source_file) as f:
@@ -67,14 +78,42 @@ class CaculateDateAndSave(object):
             lon = row[-1]
             lat = row[-2]
             loca = row[3]
+            province = row[2]
+            ethnic = row[0]
+
             haplogroup_classfy = str(row[4] or row[6])
             if not haplogroup_classfy:
                 continue
             d_haplogroup_classfy = [haplogroup_classfy[0:i + 1] for i in range(len(haplogroup_classfy))]
-
             for haplo in d_haplogroup_classfy:
                 self.lat_haplogroup_dict[haplo][lat] += 1
                 self.lon_haplogroup_dict[haplo][lon] += 1
+                self.ethnic_dict[ethnic][haplo] += 1
+
+                province_list = self.province_dict.keys()
+                province_key_list = map(lambda x: x[0], province_list)
+
+                key = (province, lon, lat)
+                if province in province_key_list:
+                    key = province_list[province_key_list.index(province)]
+                if province == '\xe2\x80\x8b\xe2\x80\x8b\xe6\xb1\x9f\xe8\x8b\x8f':
+                    key = province_list[province_key_list.index('\xe6\xb1\x9f\xe8\x8b\x8f')]
+                self.province_dict[key][haplo] += 1
+
+    def static_area(self, output_file):
+        self.get_haplogroup_amount()
+        for (province, haplogroup) in self.province_dict.items():
+            for (haplo, number) in haplogroup.items():
+                if province[0] in self.SOUTHERN_AREA:
+                    self.area_dict[haplo]['southern'] += number
+                else:
+                    self.area_dict[haplo]['northern'] += number
+
+            with open(output_file, 'wb') as f:
+                w = csv.writer(f)
+                for haplo, area in self.area_dict.items():
+                    w.writerow([haplo, area['northern'], area['southern'], (area['northern'] + area['southern'])])
+
 
     def write_to_file(self):
         self.get_haplogroup_amount()
@@ -130,6 +169,24 @@ class CaculateDateAndSave(object):
             for (lon, number) in lon_list.items():
                 self.normalization_lon_haplogroup_dict[haplo][lon] = float(number) / time[lon]
 
+    def normalization_by_province(self):
+        cnt_dict = defaultdict(int)
+        for (province, haplogroup) in self.province_dict.items():
+            for (haplo, number) in haplogroup.items():
+                if len(haplo) > 1:
+                    continue
+                cnt_dict[province[0]] += number
+
+        max_cnt = max(cnt_dict.values())
+        print max_cnt
+
+        time = dict((province, float(cnt) / max_cnt) for province, cnt in cnt_dict.items())
+
+        # print time
+        for (province, haplogroup) in self.province_dict.items():
+            for (haplo, number) in haplogroup.items():
+                self.normalization_province_dict[province][haplo] = float(number) / time[province[0]]
+
     def get_dealed_lat_date(self):
         lat_list = self.normalization_by_lat().keys()
         lon_list = self.normalization_by_lon().keys()
@@ -154,26 +211,148 @@ class CaculateDateAndSave(object):
                 cnt_dict[lon][haplo] = distributed
         return cnt_dict
 
+    def output_province_date(self, output_file):
+        SCHEAM = ['O', 'C', 'N', 'Q', 'R', 'D', 'H', 'E', 'G', 'J']
+        North_China = ['北京','天津','河北','陕西','山西','山东','内蒙古']
+        North_East = ['辽宁','吉林','黑龙江']
+        North_West = ['甘肃','青海','宁夏','新疆','西藏']
+        South_East = ['重庆','四川','贵州','云南']
+        East_China = ['上海','江苏','浙江','安徽','福建','江西','台湾']
+        Middle_China = ['河南','湖北','湖南','广东','广西','海南','香港','澳门']
+
+        North_G = ['辽宁','吉林','黑龙江']
+        Beijing_G = ['北京','天津','河北']
+        Lujing_G = ['山东']
+        Jing = ['陕西','山西']
+        Zhongyuan_G = ['河南','湖北','陕西']
+        Xinan_G = ['重庆','四川','贵州','云南','广西','青海']
+        JianHuai_G = ['江苏']
+        Wu = ['上海','江苏','浙江']
+        Xian = ['湖南']
+        Gan = ['江西']
+        Ming = ['福建']
+        Yue = ['广东']
+        Kejian = ['江西','福建']
+
+        with open(output_file ,"wb") as f:
+            area_dict = defaultdict(lambda :defaultdict(int))
+            w = csv.writer(f)
+            w.writerow(['地区','省份','总量'] + SCHEAM)
+            self.get_haplogroup_amount()
+            for province, haplo_dict in self.province_dict.items():
+                province = province[0]
+                if province in North_G:
+                    area = '官话东北'
+                elif province in Beijing_G:
+                    area = '官话北京'
+                elif province in Lujing_G:
+                    area = '官话鲁晋'
+                elif province in Jing:
+                    area = '晋语'
+                elif province in Zhongyuan_G:
+                    area = '中原官话'
+                elif province in Xinan_G:
+                    area = '西南官话'
+                elif province in JianHuai_G:
+                    area = '江淮官话'
+                elif province in Wu:
+                    area = '吴语'
+                elif province in Xian:
+                    area = '湘语'
+                elif province in Gan:
+                    area = '赣语'
+                elif province in Yue:
+                    area = '粤语'
+                elif province in Ming:
+                    area = '闽语'
+                elif province in Kejian:
+                    area = '客家'
+                else:
+                    area = '吴语'
+
+                print area_dict[area]
+                area_dict[area] = defaultdict(int, dict(Counter(area_dict[area] if len(area_dict[area]) > 0 else None) + Counter(haplo_dict)))
+
+            for area, haplo_dict in area_dict.items():
+                print area
+                print [haplo_dict[haplo] for haplo in SCHEAM]
+                print "====================="
+                haplo_sum = sum([haplo_dict[haplo] for haplo in SCHEAM])
+                w.writerow([area, haplo_sum] + [float(haplo_dict[haplo] * 10000 / haplo_sum) / 100 for haplo in SCHEAM] )
+
+    def output_ethnic_date(self, output_file):
+        self.get_haplogroup_amount()
+        SCHEAM = ['O', 'C', 'N', 'Q', 'R', 'D', 'H', 'E', 'G', 'J']
+        with open(output_file ,"wb") as f:
+            area_dict = defaultdict(lambda :defaultdict(int))
+            w = csv.writer(f)
+            w.writerow(['民族','总量'] + SCHEAM)
+
+            for ethnic, haplo_dict in self.ethnic_dict.items():
+                    print ethnic
+                    print [haplo_dict[haplo] for haplo in SCHEAM]
+                    print "====================="
+                    haplo_sum = sum([haplo_dict[haplo] for haplo in SCHEAM])
+                    w.writerow([ethnic, haplo_sum] + [float(haplo_dict[haplo] * 10000 / haplo_sum) / 100 for haplo in SCHEAM] )
+
     def stacked_draw(self):
-        N = 5
-        menMeans = (20, 35, 30, 35, 27)
-        womenMeans = (25, 32, 34, 20, 25)
-        menStd = (2, 3, 4, 1, 2)
-        womenStd = (3, 5, 2, 3, 3)
-        ind = np.arange(N)    # the x locations for the groups
-        width = 0.35       # the width of the bars: can also be len(x) sequence
+        self.get_haplogroup_amount()
+        self.normalization_by_province()
+        self.province_dict = self.normalization_province_dict
+        N = len(self.province_dict)
+        lon_list = sorted(self.province_dict.keys(), key=lambda x: float(x[1]))
+        map_lon_list = map(lambda x: x.decode("utf-8"), map(lambda x: x[0], lon_list))
 
-        p1 = plt.bar(ind, menMeans, width, color='r', yerr=menStd)
-        p2 = plt.bar(ind, womenMeans, width, color='y',
-                     bottom=menMeans, yerr=womenStd)
+        # lat_list = sorted(self.province_dict.keys(), key=lambda x: float(x[2]))
+        # map_lat_list = map(lambda x: x.decode("utf-8"), map(lambda x: x[0], lat_list))
 
-        plt.ylabel('Scores')
-        plt.title('Scores by group and gender')
-        plt.xticks(ind + width/2., ('G1', 'G2', 'G3', 'G4', 'G5'))
-        plt.yticks(np.arange(0, 81, 10))
-        plt.legend((p1[0], p2[0]), ('Men', 'Women'))
+        haplo_number_dict = defaultdict(int)
+        for (province, haplo_dict) in self.province_dict.items():
+            for (haplo, number) in haplo_dict.items():
+                haplo_number_dict[haplo] += number
 
+        haplo_list = map(lambda x: x[0], sorted(haplo_number_dict.items(), key=lambda x: x[1], reverse=True))
+        colors = ['c', 'y', 'm', 'r', 'mediumseagreen', 'crimson', 'orange', 'teal', 'mediumpurple', 'indigo']
+
+        ind = np.arange(N)  # the x locations for the groups
+        width = 1  # the width of the bars: can also be len(x) sequence
+        p_list = list()
+        stacked_list = [[0] * len(lon_list)]
+        select_haplo = list()
+        # print len(self.province_dict)
+        for haplo in haplo_list:
+            # if len(haplo) != 3 or haplo[0] != "O":
+            #     continue
+            if len(haplo) > 1:
+                continue
+            select_haplo.append(haplo)
+            tmp_list = list()
+            for province in lon_list:
+                # print province, haplo
+                tmp_list.append(self.province_dict[province][haplo])
+            # print tmp_list
+
+            if haplo_list.index(haplo) == 0:
+                p_list.append(plt.bar(ind, tmp_list, width, color=colors.pop()))
+                # break
+            else:
+                p_list.append(plt.bar(ind, tmp_list, width, color=colors.pop(), bottom=map(sum, zip(*stacked_list))))
+            stacked_list.append(tmp_list)
+        plt.ylabel('Quantity')
+        plt.xlabel('Province')
+        plt.title('Composition Of Haplogroup  Sort by Longitude(Normalization) %s' % "", fontsize=21, ha='center', va='bottom',
+                  style="oblique")
+        plt.xticks(ind + width / 2., map_lon_list)
+        locs, labels = plt.xticks()
+        plt.setp(labels, rotation=90)
+        max_ytick = max(map(sum, zip(*stacked_list)))
+        plt.yticks(np.arange(0, max_ytick, 10))
+        plt.legend([p[0] for p in p_list], select_haplo, loc='center left', bbox_to_anchor=(1, 0.5),
+                   ncol=1, fancybox=True, shadow=True)
+        plt.draw()
+        plt.savefig('%s_composition.png' % "normalization_lon", bbox_inches='tight')
         plt.show()
+        plt.close()
 
     def draw(self):
         self.get_haplogroup_amount()
@@ -267,7 +446,7 @@ class CaculateDateAndSave(object):
         axLable.legend(handles=patch_list, mode="expand", ncol=2, shadow=True)
         axHistx.set_title('Distribution Of Haplogroup %s' % "", fontsize=21, ha='center', va='bottom', style="oblique")
         plt.draw()
-        plt.savefig('%s_distributed.png' % "",  bbox_inches='tight')
+        plt.savefig('%s_distributed.png' % "", bbox_inches='tight')
         plt.show()
         plt.close()
 
@@ -549,8 +728,43 @@ class SpiltData(object):
     def draw_lon_haplogroup_distributed(self):
         pass
 
+class Arlequin(object):
+    def __init__(self, infile, outfile):
+        self.infile = infile
+        self.outfile = outfile
+
+    def output_province(self):
+        w = open(self.outfile, 'wb')
+        with open(self.infile) as f:
+            for l in f:
+                spilter = l.split(',')
+                print spilter
+                # if spilter[0] == '':
+                #     continue
+                # if spilter[1] == '省份':
+                #     continue
+                w.write('SampleName="%s"\n' % spilter[0])
+                w.write('SampleSize=%s\n' % spilter[1])
+                print spilter
+                print [float(spilter[i + 2]) for i in xrange(10)]
+                total = sum([float(spilter[i + 2]) for i in xrange(10)])
+                w.write('SampleData= {\n')
+                w.write('   1   %s\n' % (float(spilter[2]) / total))
+                w.write('   2   %s\n' % (float(spilter[3]) / total))
+                w.write('   3   %s\n' % (float(spilter[4]) / total))
+                w.write('   4   %s\n' % (float(spilter[5]) / total))
+                w.write('   5   %s\n' % (float(spilter[6]) / total))
+                w.write('   6   %s\n' % (float(spilter[7]) / total))
+                w.write('   7   %s\n' % (float(spilter[8]) / total))
+                w.write('   8   %s\n' % (float(spilter[9]) / total))
+                w.write('   9   %s\n' % (float(spilter[10]) / total))
+                w.write('   10   %s\n' % (float(spilter[11]) / total))
+                w.write('}\n')
+                # w.write('"' + spilter[1] + '"\n')
+        w.close()
+
 
 if __name__ == "__main__":
     # GetGip().get_and_save()
     # SpiltData('data1.csv').inter()
-    CaculateDateAndSave('data1.csv', 'haplo_quality').draw()
+    Arlequin('Mandarin.csv','arl_Mandarin.txt').output_province()
